eeeb750f55def453b8914c7033d9f3a11068acf7
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2023-12-08 13:30:33 +0100

    Do not parse options in OptionsHandler

--- cmd/connect.go
+++ cmd/connect.go
@@ -50,9 +50,6 @@ const (
 //   }
 // ]
 
-// RemoteAccessTargets is a list of remote access targets.
-var RemoteAccessTargets = make([]client.RemoteAccessConnection, 0)
-
 var connectCommand = Command{
 	Description: "Connect to a device",
 	Options: []Option{
@@ -74,19 +71,6 @@ var connectCommand = Command{
 	},
 	OptionsHandler: func(opts Options) error {
 		if opts[connectConfigFileOption] != "" {
-			configBytes, err := os.ReadFile(opts[connectConfigFileOption])
-			if err != nil {
-				return fmt.Errorf("error reading config file: %w", err)
-			}
-
-			err = json.Unmarshal(configBytes, &RemoteAccessTargets)
-			if err != nil {
-				return fmt.Errorf("error parsing config file: %w", err)
-			}
-
-			if len(RemoteAccessTargets) == 0 {
-				return fmt.Errorf("no connections defined in config file")
-			}
 			return nil
 		}
 		if opts[connectDeviceOption] == "" {
@@ -95,10 +79,6 @@ var connectCommand = Command{
 		if opts[connectTargetOption] == "" {
 			return fmt.Errorf("missing target")
 		}
-		RemoteAccessTargets = append(RemoteAccessTargets, client.RemoteAccessConnection{
-			DeviceID: opts[connectDeviceOption],
-			Targets:  strings.Split(opts[connectTargetOption], ","),
-		})
 		return nil
 	},
 	Target: func(opts Options) error {
@@ -106,26 +86,6 @@ var connectCommand = Command{
 		password := os.Getenv("QBEE_PASSWORD")
 
 		ctx := context.Background()
-
-		remoteAccessMap := make(map[string][]client.RemoteAccessTarget, 0)
-
-		for _, target := range RemoteAccessTargets {
-			if !client.IsValidDeviceID(target.DeviceID) {
-				return fmt.Errorf("invalid device ID %s", target.DeviceID)
-			}
-
-			targets := make([]client.RemoteAccessTarget, 0)
-			for _, targetString := range target.Targets {
-				target, err := client.ParseRemoteAccessTarget(targetString)
-				if err != nil {
-					return fmt.Errorf("error parsing target %s: %w", targetString, err)
-				}
-
-				targets = append(targets, target)
-			}
-
-			remoteAccessMap[target.DeviceID] = targets
-		}
 
 		cli := client.New()
 		if baseURL, ok := os.LookupEnv("QBEE_BASEURL"); ok {
@@ -135,6 +95,30 @@ var connectCommand = Command{
 		if err := cli.Authenticate(ctx, email, password); err != nil {
 			return err
 		}
-		return cli.Connect(ctx, remoteAccessMap)
+
+		remoteAccessTargets := make([]client.RemoteAccessConnection, 0)
+
+		if opts[connectConfigFileOption] != "" {
+			configBytes, err := os.ReadFile(opts[connectConfigFileOption])
+			if err != nil {
+				return fmt.Errorf("error reading config file: %w", err)
+			}
+
+			if err := json.Unmarshal(configBytes, &remoteAccessTargets); err != nil {
+				return fmt.Errorf("error parsing config file: %w", err)
+			}
+
+			if len(remoteAccessTargets) == 0 {
+				return fmt.Errorf("no connections defined in config file")
+			}
+		} else {
+			remoteAccessTargets = append(remoteAccessTargets, client.RemoteAccessConnection{
+				DeviceID: opts[connectDeviceOption],
+				Targets:  strings.Split(opts[connectTargetOption], ","),
+			})
+		}
+		cli.ConnectMulti(ctx, remoteAccessTargets)
+		return nil
+
 	},
 }
