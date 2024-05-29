11f61007f3beb288f9f440e55d13a7d0aef8a473
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2024-05-29 12:20:27 +0200

    Simple config save, commit and show

--- configmanager.go
+++ configmanager.go
@@ -16,36 +16,135 @@
 
 package client
 
-import "context"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"text/template"
 
-type ConfiguratinManager struct {
+	"go.qbee.io/client/config"
+)
+
+type ConfigurationManager struct {
 	// client is the ConfiguratinManager client.
 	client *Client
+
+	// templateParameters are the template parameters.
+	templateParameters map[string]string
+
+	// entityType is the entity type.
+	entityType config.EntityType
+
+	// entityConfigScope is the entity scope.
+	entityConfigScope config.EntityConfigScope
 }
 
-// uploadConfiguration uploads configuration of any type to the entity.
-func (c *ConfiguratinManager) uploadConfiguration(ctx context.Context, entityID string, config []byte) error {
-
-	return nil
+// NewConfigurationManager creates a new ConfigurationManager.
+func NewConfigurationManager() *ConfigurationManager {
+	return &ConfigurationManager{}
 }
 
-// commitConfiguration commits the configuration for the entity.
-func (c *ConfiguratinManager) commitConfiguration(ctx context.Context, entityID string) error {
-
-	return nil
+// WithClient sets the client.
+func (c *ConfigurationManager) WithClient(client *Client) *ConfigurationManager {
+	c.client = client
+	return c
 }
 
-// UploadAndCommitConfiguration uploads and commits the configuration for the entity.
+// WithTemplateParameters sets the template parameters.
+func (c *ConfigurationManager) WithTemplateParameters(params map[string]string) *ConfigurationManager {
+	c.templateParameters = params
+	return c
+}
 
-func (c *ConfiguratinManager) UploadAndCommitConfiguration(ctx context.Context, commitMessage, entityID string, config []byte) error {
+// WithEntityType sets the entity type.
+func (c *ConfigurationManager) WithEntityType(entityType config.EntityType) *ConfigurationManager {
+	c.entityType = entityType
+	return c
+}
 
-	if err := c.uploadConfiguration(ctx, entityID, config); err != nil {
+// WithEntityConfigScope sets the entity config scope.
+func (c *ConfigurationManager) WithEntityConfigScope(entityConfigScope config.EntityConfigScope) *ConfigurationManager {
+	c.entityConfigScope = entityConfigScope
+	return c
+}
+
+// Save saves the configuration for the entity type.
+func (c *ConfigurationManager) Save(ctx context.Context, target, formType, configFile string) error {
+
+	var configBytes []byte
+	var err error
+	var configData any
+
+	if configBytes, err = os.ReadFile(configFile); err != nil {
 		return err
 	}
 
-	if err := c.commitConfiguration(ctx, commitMessage); err != nil {
+	if c.templateParameters != nil {
+		if configBytes, err = c.expandTemplate(configBytes); err != nil {
+			return err
+		}
+	}
+
+	if err = json.Unmarshal(configBytes, &configData); err != nil {
 		return err
 	}
 
+	configPayload := &ConfigPayload{
+		FormType: formType,
+		Config:   configData,
+	}
+
+	if c.entityType == config.EntityTypeNode {
+		configPayload.NodeID = target
+	} else if c.entityType == config.EntityTypeTag {
+		configPayload.Tag = target
+	} else {
+		return fmt.Errorf("invalid entity type: %s", c.entityType)
+	}
+
+	if err := c.client.UploadConfig(ctx, configPayload); err != nil {
+		return err
+	}
 	return nil
+
+}
+
+// Commit commits the configuration if there are uncommitted changes.
+func (c *ConfigurationManager) Commit(ctx context.Context, commitMessage string) error {
+	if err := c.client.CommitConfig(ctx, commitMessage); err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetConfig returns the active configuration for the entity.
+func (c *ConfigurationManager) GetConfig(
+	ctx context.Context,
+	entityID string,
+) (*config.Config, error) {
+
+	return c.client.GetActiveConfig(ctx, c.entityType, entityID, c.entityConfigScope)
+}
+
+// expandTemplate expands the template using built in Golang templating.
+func (c *ConfigurationManager) expandTemplate(configBytes []byte) ([]byte, error) {
+
+	tmpl, err := template.New("config-template").Parse(string(configBytes))
+
+	// Produce error on missing keys.
+	tmpl.Option("missingkey=error")
+
+	if err != nil {
+		return nil, err
+	}
+
+	buf := new(bytes.Buffer)
+
+	if err := tmpl.Execute(buf, c.templateParameters); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
