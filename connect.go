14c816812576113120ec2f324f0f63a38815d732
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2026-01-20 15:04:49 +0100

    remove all legacy remote access

--- connect.go
+++ connect.go
@@ -25,8 +25,6 @@ import (
 	"math"
 	"math/rand/v2"
 	"net"
-	"net/http"
-	"net/url"
 	"os"
 	"strings"
 	"sync"
@@ -35,140 +33,8 @@ import (
 	"go.qbee.io/client/console"
 	"go.qbee.io/transport"
 
-	chisel "github.com/jpillora/chisel/client"
-	"golang.org/x/net/http/httpproxy"
 	"golang.org/x/term"
 )
-
-const (
-	remoteAccessKeepAlive     = 25 * time.Second
-	remoteAccessLegacyURL     = "%s/qbee-connect/%s/"
-	remoteAccessInterfaceName = "qbee0"
-)
-
-// RemoteAccessConnectionInfo contains parameters used to establish a remote access connection.
-type RemoteAccessConnectionInfo struct {
-	// URL of the remote access server. If empty, a legacy URL will be used.
-	URL string `json:"url"`
-
-	// VPNIndex is the legacy VPN index to use for the connection.
-	VPNIndex string `json:"vpn_idx"`
-
-	// Username is the username to use for authentication.
-	Username string `json:"username"`
-
-	// Password is the password to use for authentication.
-	Password string `json:"password"`
-
-	// Address is the internal device address client is connecting to.
-	Address string `json:"address"`
-}
-
-// RemoteAccessToken contains remote console
-type RemoteAccessToken struct {
-	// Title is the title of the device.
-	Title string `json:"title"`
-
-	// Connection contains the connection details.
-	Connection RemoteAccessConnectionInfo `json:"connection"`
-}
-
-// AuthString returns the authentication string for the remote access token.
-func (token RemoteAccessToken) AuthString() string {
-	return fmt.Sprintf("%s:%s", token.Connection.Username, token.Connection.Password)
-}
-
-// ProxyURL returns the proxy URL for the remote access token.
-func (token RemoteAccessToken) ProxyURL() (string, error) {
-	serverURL, err := url.Parse(token.Connection.URL)
-	if err != nil {
-		return "", fmt.Errorf("error parsing remote access server URL: %w", err)
-	}
-
-	var proxyURL *url.URL
-	if proxyURL, err = httpproxy.FromEnvironment().ProxyFunc()(serverURL); err != nil {
-		return "", fmt.Errorf("error getting proxy URL: %w", err)
-	}
-
-	if proxyURL == nil {
-		return "", nil
-	}
-
-	return proxyURL.String(), nil
-}
-
-// RemoteAccessTokenResponse is the response returned by the API when requesting a remote console token.
-type RemoteAccessTokenResponse map[string]RemoteAccessToken
-
-// RemoteAccessTokenRequest is the request sent to the API when requesting a remote console token.
-type RemoteAccessTokenRequest struct {
-	// DeviceID is the PublicKeyDigest of the device for which the token is requested.
-	// Required.
-	DeviceID string
-
-	// Application is the name of the application for which the token is requested.
-	// Optional - defaults to "qbee-cli".
-	Application string
-
-	// Username is the username for which the token is requested.
-	// Setting this value will allow to identify the user in the audit log.
-	// Optional.
-	Username string
-
-	// Ports is the list of ports for which the token is requested.
-	// Setting this value will allow to identify requested ports in the audit log.
-	// Optional.
-	Ports []string
-}
-
-// remoteAccessTokenV2Path is the path of the remote access token API.
-// First argument is the device ID, second argument is the URL-encoded query string.
-const remoteAccessTokenV2Path = "/api/v2/remoteconsoletokenv2/%s?%s"
-
-// GetRemoteAccessToken returns a remote console token for the specified device.
-func (cli *Client) GetRemoteAccessToken(ctx context.Context, req RemoteAccessTokenRequest) (*RemoteAccessToken, error) {
-	if req.DeviceID == "" {
-		return nil, fmt.Errorf("device ID is required")
-	}
-
-	if req.Application == "" {
-		req.Application = Name
-	}
-
-	urlValues := url.Values{}
-	urlValues.Set("app_name", req.Application)
-
-	if req.Username != "" {
-		urlValues.Set("username", req.Username)
-	}
-
-	if len(req.Ports) > 0 {
-		jsonEncodedPorts, err := json.Marshal(req.Ports)
-		if err != nil {
-			return nil, fmt.Errorf("error encoding ports: %w", err)
-		}
-
-		urlValues.Set("ports", string(jsonEncodedPorts))
-	}
-
-	path := fmt.Sprintf(remoteAccessTokenV2Path, req.DeviceID, urlValues.Encode())
-
-	response := make(RemoteAccessTokenResponse)
-
-	if err := cli.Call(ctx, http.MethodGet, path, nil, &response); err != nil {
-		return nil, err
-	}
-
-	remoteAccessToken := response[req.DeviceID]
-
-	// for legacy remote access servers, set the URL based on the VPN index
-	if remoteAccessToken.Connection.URL == "" {
-		remoteAccessToken.Connection.URL = fmt.Sprintf(
-			remoteAccessLegacyURL, cli.baseURL, remoteAccessToken.Connection.VPNIndex)
-	}
-
-	return &remoteAccessToken, nil
-}
 
 // ConnectMulti establishes connections to multiple remote devices concurrently.
 func (cli *Client) ConnectMulti(ctx context.Context, connections []RemoteAccessConnection, allowFailures bool) error {
@@ -409,73 +275,6 @@ func (cli *Client) connect(ctx context.Context, deviceUUID, edgeHost string, tar
 	}
 }
 
-// legacyConnect establishes a connection to a remote device using the legacy remote access solution.
-func (cli *Client) legacyConnect(ctx context.Context, deviceID string, targets []RemoteAccessTarget) error {
-	ports := make([]string, len(targets))
-	for _, target := range targets {
-		// only localhost is supported as remote host for legacy remote access
-		if target.RemoteHost != "localhost" {
-			return fmt.Errorf("invalid remote host: only localhost is supported")
-		}
-
-		ports = append(ports, fmt.Sprintf("%s:%s", target.Protocol, target.RemotePort))
-	}
-
-	remoteAccessTokenRequest := RemoteAccessTokenRequest{
-		DeviceID: deviceID,
-		Ports:    ports,
-	}
-
-	remoteAccessToken, err := cli.GetRemoteAccessToken(ctx, remoteAccessTokenRequest)
-	if err != nil {
-		return err
-	}
-
-	var proxyURL string
-	if proxyURL, err = remoteAccessToken.ProxyURL(); err != nil {
-		return fmt.Errorf("error getting proxy URL: %w", err)
-	}
-
-	address := remoteAccessToken.Connection.Address
-	if address == "" {
-		var deviceInventory *DeviceInventory
-		if deviceInventory, err = cli.GetDeviceInventory(ctx, deviceID); err != nil {
-			return fmt.Errorf("error getting device inventory: %w", err)
-		}
-
-		address = deviceInventory.SystemInfo.IPv4[remoteAccessInterfaceName]
-	}
-
-	remotes := make([]string, len(targets))
-	for i, target := range targets {
-		target.RemoteHost = address
-		remotes[i] = target.String()
-	}
-
-	chiselClientConfig := &chisel.Config{
-		Auth:      remoteAccessToken.AuthString(),
-		Server:    remoteAccessToken.Connection.URL,
-		Proxy:     proxyURL,
-		KeepAlive: remoteAccessKeepAlive,
-		Remotes:   remotes,
-	}
-
-	var chiselClient *chisel.Client
-	if chiselClient, err = chisel.NewClient(chiselClientConfig); err != nil {
-		return fmt.Errorf("error initializing remote access client: %w", err)
-	}
-
-	chiselClient.Info = false
-	if err = chiselClient.Start(ctx); err != nil {
-		return err
-	}
-
-	if err = chiselClient.Wait(); err != nil {
-		return err
-	}
-	return nil
-}
-
 // Connect establishes a connection to a remote device.
 func (cli *Client) Connect(ctx context.Context, deviceID string, targets []RemoteAccessTarget) error {
 	deviceStatus, err := cli.GetDeviceStatus(ctx, deviceID)
@@ -488,8 +287,6 @@ func (cli *Client) Connect(ctx context.Context, deviceID string, targets []Remot
 	}
 
 	switch deviceStatus.EdgeVersion {
-	case EdgeVersionOpenVPN:
-		return cli.legacyConnect(ctx, deviceID, targets)
 	case EdgeVersionNative:
 		return cli.connect(ctx, deviceStatus.UUID, deviceStatus.Edge, targets)
 	default:
--- go.mod
+++ go.mod
@@ -3,21 +3,9 @@ module go.qbee.io/client
 go 1.25.5
 
 require (
-	github.com/jpillora/chisel v1.11.3
 	github.com/xtaci/smux v1.5.44
 	go.qbee.io/transport v1.25.28
-	golang.org/x/net v0.47.0
 	golang.org/x/term v0.37.0
 )
 
-require (
-	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5 // indirect
-	github.com/fsnotify/fsnotify v1.9.0 // indirect
-	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/jpillora/backoff v1.0.0 // indirect
-	github.com/jpillora/sizestr v1.0.0 // indirect
-	golang.org/x/crypto v0.45.0 // indirect
-	golang.org/x/sync v0.18.0 // indirect
-	golang.org/x/sys v0.38.0 // indirect
-	golang.org/x/text v0.31.0 // indirect
-)
+require golang.org/x/sys v0.38.0 // indirect
